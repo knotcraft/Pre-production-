6b1eebf74b24c87f147a9b4b82ded43459355244
Albin Shaji <albinalby24@gmail.com>
2026-01-25 18:05:48 +0000

    Make it more convenient design  (_for element div_)

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -4,7 +4,7 @@ import { BottomNav } from '@/components/layout/bottom-nav';
 export function AppShell({ children }: { children: ReactNode }) {
   return (
     <div className="text-foreground transition-colors duration-300">
-      <div className="relative mx-auto flex min-h-screen max-w-md flex-col overflow-x-hidden bg-background shadow-2xl pb-24">
+      <div className="relative mx-auto flex min-h-screen max-w-md flex-col overflow-x-hidden bg-background shadow-2xl pb-28">
         <main>{children}</main>
         <BottomNav />
       </div>
